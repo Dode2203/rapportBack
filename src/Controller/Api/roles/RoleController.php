21448851d2bf617650e0b580e479764094061af2
Antonio2772 <atn.randr@gmail.com>
2026-02-27 16:14:51 +0300

    feat: API for Role

---
+++ src/Controller/Api/roles/RoleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Api\roles;
+
+use App\Controller\Api\utils\BaseApiController;
+use App\Repository\utilisateurs\RolesRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Annotation\TokenRequired;
+
+#[Route('/roles')]
+class RoleController extends BaseApiController
+{
+    #[Route('', name: 'api_roles_index', methods: ['GET'])]
+    #[TokenRequired(['Admin'])]
+    public function index(RolesRepository $roleRepository): JsonResponse
+    {
+        try {
+            $roles = $roleRepository->findAll();
+
+            $data = array_map(function ($role) {
+                return [
+                    'id' => $role->getId(),
+                    // 'code' => 'ROLE_' . strtoupper($role->getName()),
+                    'nom' => $role->getName(),
+                ];
+            }, $roles);
+
+            return $this->jsonSuccess($data);
+        } catch (\Throwable $e) {
+            return $this->jsonError($e->getMessage(), 400);
+        }
+    }
+}
